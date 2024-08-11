caa612c58936a83c10dab784631dd5829a2b35d0
Awes Khan <44608315+Awes-Khan@users.noreply.github.com>
2024-08-11 23:38:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
 // your code here
+	let n1=0;
+	let n2=1;
+	if(num == 1) return 0;
+	if(num == 2) return 1;
+	let i=2;
+	while(i!=num){
+		let temp = n2;
+		n2 = n1+n2;
+		n1 = temp;
+		i++;
+	}
+	return n2;
+	
 }
 
 module.exports = fibonacci;
